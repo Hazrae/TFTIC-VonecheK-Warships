f6836624fd4f5a1c28cb4efff6df01a63ab4b07b
IraVehementi <kevinvoneche@gmail.com>
2020-05-18 18:01:01 +0200

    Modif procédure stockée, gestion erreur ok

--- ProjectWarshipDB/User/StoredProcedures/UpdatePassword.sql
+++ ProjectWarshipDB/User/StoredProcedures/UpdatePassword.sql
@@ -1,20 +1,18 @@
 ﻿CREATE PROCEDURE [dbo].[UpdatePassword]
 	@id int,
 	@oldpassword nvarchar(25),
-	@password nvarchar(25)
-	
-
+	@password nvarchar(25)	
 AS
 BEGIN
 	IF(SELECT COUNT(*) 
 	   FROM [User]  
-       WHERE password = HASHBYTES('SHA2_512',dbo.Salt() +@oldpassword)) = 0
-		  return 1;
+       WHERE password = HASHBYTES('SHA2_512',dbo.Salt() +@oldpassword) AND id = @id) = 0
+		Select 1
 	ELSE
 	BEGIN	
 		UPDATE [User] 
 		SET [password] = HASHBYTES('SHA2_512',dbo.Salt() +@password)
 		WHERE id = @id
-		return 0;
+		Select 0
 	END
 END
